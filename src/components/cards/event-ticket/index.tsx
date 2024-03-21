7a610c55b9896f3995dec2724d4355b493889ff2
Izael <izaelf32@gmail.com>
2024-03-21 17:24:14 -0300

    Adicionado cartão de ticket

---
+++ src/components/cards/event-ticket/index.tsx
@@ -0,0 +1,24 @@
+import './index.scss'
+
+export function EventTicket() {
+    return (
+        <main id="event-ticket">
+            <span>date: </span>
+
+            <section className='middle'>
+                <div className='circle'></div>
+                <h1>admit two</h1>
+                <p>12 lorem ipsum center plaza</p>
+                <p>city, state</p>
+                <p>11 00 68</p>
+            </section>
+
+            <section className='last'>
+                <p>no. 516783 a</p>
+                <p>when: 22 // 09 // 2022</p>
+                <p>seat: 22 f</p>
+                <span>lorem ipsum text here</span>
+            </section>
+        </main>
+    )
+}
--- src/pages/cards/index.tsx
+++ src/pages/cards/index.tsx
@@ -5,9 +5,10 @@ import {
   } from "@/components/ui/dialog"
 
 import { NeoBrutalist } from '@/components/cards/neo-brutalist'
+import { CardCoffee } from "@/components/cards/card-coffee"
+import { EventTicket } from "@/components/cards/event-ticket"
 
 import './index.scss'
-import { CardCoffee } from "@/components/cards/card-coffee"
 
 export function Cards() {
     return (
@@ -30,9 +31,22 @@ export function Cards() {
                         <CardCoffee />
                     </DialogTrigger>
 
-                    <DialogContent className="size-[70%]">
+                    <DialogContent className="w-[60vh] h-[70vh]">
                         <CardCoffee />
                         <p className="inspiration">Inspiração: <a href="https://br.pinterest.com/pin/7318418139237081" target="_blank">https://br.pinterest.com/pin/7318418139237081</a></p>
+                    </DialogContent>
+                </Dialog>
+            </section>
+
+            <section>
+                <Dialog>
+                    <DialogTrigger className="size-full">
+                        <EventTicket />
+                    </DialogTrigger>
+
+                    <DialogContent className="w-[55vh] h-[75vh]">
+                        <EventTicket />
+                        <p className="inspiration">Inspiração: <a href="https://br.pinterest.com/pin/981995893725168092" target="_blank">https://br.pinterest.com/pin/981995893725168092</a></p>
                     </DialogContent>
                 </Dialog>
             </section>
