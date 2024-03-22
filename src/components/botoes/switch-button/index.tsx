fbfcfbe9b0f6235aa0663d4cf0c26217b487c998
Izael <izaelf32@gmail.com>
2024-03-22 17:12:00 -0300

    Alterando o sistema input com checkbox para o useState do botão switch

--- src/components/botoes/switch-button/index.tsx
+++ src/components/botoes/switch-button/index.tsx
@@ -1,13 +1,19 @@
+import { useState } from 'react'
+
 import './index.scss'
 
 export function SwitchButton(){
+    const [switchColor, setSwitchColor] = useState(false)
+
+    function handleSwitch(){
+        setSwitchColor(!switchColor)
+    }
+
     return (
         <main id="switch-button">
-            <input id='switch' type="checkbox" />
-            
-            <label htmlFor='switch'>
-                <span></span>
-            </label>
+            <section onClick={ handleSwitch }>
+                <span className={ switchColor ? 'switch-color' : 'switch-color2' }></span>
+            </section>
         </main>
     )
 }
